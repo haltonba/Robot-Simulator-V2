1332dc05bc83021541959669562f3b88a7d16e44
haltonba <haltonba94@gmail.com>
2020-03-12 11:59:23 -0400

    Ricky/Halton

--- index.js
+++ index.js
@@ -2,5 +2,45 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  const movesCon = document.getElementById("moves-container");
+
+  document.addEventListener("keydown", function(event) {
+    switch (event.keyCode) {
+      case 37:
+           let left = document.createElement("li");
+           left.innerText = "left";
+           movesCon.appendChild(left);
+         break;
+      case 38:
+          let up = document.createElement("li");
+          up.innerText = "up";
+          movesCon.appendChild(up);
+         break;
+      case 39:
+          let right = document.createElement("li");
+          right.innerText = "right";
+          movesCon.appendChild(right);
+         break;
+      case 40:
+          let down = document.createElement("li");
+          down.innerText = "down";
+          movesCon.appendChild(down);
+         break;
+      case 8:
+        movesCon.removeChild(document.getElementById("moves-container").lastChild);
+         break;
+   }
+  });
+
+  let moveButton = document.getElementById("move-button");
+
+  moveButton.addEventListener("click", function (event) {
+    let movesArray = Array.from(document.getElementsByTagName("li"));
+    let current = movesArray[0];
+    move(current.innerText);
+    movesCon.removeChild(current);
+  });
+
+
+  
 })
